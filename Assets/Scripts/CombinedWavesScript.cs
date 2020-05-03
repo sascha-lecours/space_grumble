77aca8ce840780cf98b0cfe47b611c040e83aab9
sascha-lecours <sascha.lecours@gmail.com>
2020-05-03 13:05:19 -0400

    new wave-combiner object for multi-wave level elements added. New waves.

---
+++ Assets/Scripts/CombinedWavesScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinedWavesScript : MonoBehaviour
+{
+    public Transform[] waveSpawners = null;
+
+    // Instantiate all waveSpawners, then destroy self.
+    void Start()
+    {
+        for (int i = 0; i < waveSpawners.Length; i++)
+        {
+            Instantiate(waveSpawners[i]);
+        }
+        Destroy(gameObject);
+    }
+}
