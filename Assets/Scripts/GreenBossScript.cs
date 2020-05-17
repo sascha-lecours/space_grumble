31cf1ef0cf1c2001a421ee358e7fb24ad329b42e
sascha-lecours <sascha.lecours@gmail.com>
2020-05-17 19:49:45 -0400

    size changes to grey enemy and projectiles

---
+++ Assets/Scripts/GreenBossScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenBossScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // TODO: Spawn above screen and (remain invincible until?) reaching start point 2/3 of the way up.
+    // Movement: move horizontally from one side to the other, almost to the screen edges.
+    // Oscillate vertically just slightly, for aesthetics
+
+    // On recurring timer, shoot homing projectile cluster
+
+    // On separate recurring timer, spawn green enemies (from self, or offscreen?)
+
+    // TODO: "Warning" intro should appear before/during boss activation step.
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
