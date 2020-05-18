a0f6d47a3394c24b0043ce1d9c7b2b95dc12efc4
sascha-lecours <sascha.lecours@gmail.com>
2020-05-18 19:03:55 -0400

    Basic version of boss horizontal movement

--- Assets/Scripts/GreenBossScript.cs
+++ Assets/Scripts/GreenBossScript.cs
@@ -6,13 +6,16 @@ public class GreenBossScript : MonoBehaviour
 {
     // TODO: Remember to uncheck "active" on this gameobject's prefab when wakeup sequence is implemented
 
-    private float movementX = 0;
-    private float movementY = 0;
+    private int movementX = -1; // Should hold int value from 1 to -1
+    private int movementY = 0; // Should hold int value from 1 to -1
+    private MoveScript ms = null;
+    private Transform myTransform = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ms = GetComponent<MoveScript>();
+        myTransform = GetComponent<Transform>();
     }
 
     // TODO: Spawn above screen and (remain invincible until?) reaching start point 2/3 of the way up.
@@ -28,6 +31,26 @@ public class GreenBossScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ms != null)
+        {
+            ms.direction.x = movementX;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a bumper?
+        BumperScript bumper = otherCollider.gameObject.GetComponent<BumperScript>();
+        if (bumper != null)
+        {
+            if (movementX == -1 && bumper.bumperSide == BumperScript.Bumpers.left)
+            {
+                movementX = 1;
+            }
+            else if (movementX == 1 && bumper.bumperSide == BumperScript.Bumpers.right)
+            { 
+                movementX = -1;
+            }
+        }
     }
 }
