bfd68eefe275e165cd20fe345f4c5d7253d167a3
sascha-lecours <sascha.lecours@gmail.com>
2020-05-19 21:35:56 -0400

    implemented better lerping for movescript direction changes

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -11,20 +11,20 @@ public class MoveScript : MonoBehaviour
     
     public Vector2 speed = new Vector2(10f, 10f);
     public Vector2 direction = new Vector2(-1, 0);
-    public float acceleration = 1f; // Range from 0 to 1, 1 being instant.
-
+    public float acceleration = 1f;
     private Vector2 curSpeed = new Vector2(0f, 0f);
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
 
     private void Start()
     {
-        curSpeed = startSpeed;
+        curSpeed.x = startSpeed.x * direction.x;
+        curSpeed.y = startSpeed.y * direction.y;
     }
 
     void Update()
     {
-        curSpeed.x =  Mathf.Lerp(curSpeed.x, speed.x * direction.x, (acceleration * Time.deltaTime));
+        curSpeed.x = Mathf.Lerp(curSpeed.x, speed.x * direction.x, (acceleration * Time.deltaTime));
         curSpeed.y = Mathf.Lerp(curSpeed.y, speed.y * direction.y, (acceleration * Time.deltaTime));
 
         movement = new Vector2(
